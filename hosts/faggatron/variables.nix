6e65344bc948202f61a598d90fd6a17bf56a6400
Literally Kirby <56140289+LiterallyKirby@users.noreply.github.com>
2025-05-06 12:45:30 -0500

    Add files via upload

---
+++ hosts/faggatron/variables.nix
@@ -0,0 +1,35 @@
+{ config, lib, ... }: {
+  imports = [
+    # Choose your theme here:
+    ../../themes/nixy.nix
+  ];
+
+  config.var = {
+    hostname = "faggatron";
+    username = "kirby";
+    configDirectory = "/home/" + config.var.username
+      + "/.config/nixos"; # The path of the nixos configuration directory
+
+    keyboardLayout = "en_US";
+
+    location = "America";
+    timeZone = "America/Chicago";
+    defaultLocale = "en_US.UTF-8";
+
+    git = {
+      username = "LiteralyKirby";
+      email = "soniciscool1232@gmail.com";
+    };
+
+    autoUpgrade = false;
+    autoGarbageCollector = true;
+  };
+
+  # Let this here
+  options = {
+    var = lib.mkOption {
+      type = lib.types.attrs;
+      default = { };
+    };
+  };
+}
