aa7fb61e3e517bc765a1afc3a08904e879307ba7
Literally Kirby <56140289+LiterallyKirby@users.noreply.github.com>
2025-05-06 12:46:30 -0500

    Add files via upload

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  # https://github.com/anotherhadi/nixy
+  description = ''
+    Nixy simplifies and unifies the Hyprland ecosystem with a modular, easily customizable setup.
+    It provides a structured way to manage your system configuration and dotfiles with minimal effort. 
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
+    stylix.url = "github:danth/stylix";
+    apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    nixcord.url = "github:kaylorben/nixcord";
+    sops-nix.url = "github:Mic92/sops-nix";
+    nixarr.url = "github:rasmus-kirk/nixarr";
+    anyrun.url = "github:fufexan/anyrun/launch-prefix";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixvim = {
+      url =
+        "github:nix-community/nixvim/d81f37256d0a8691b837b74979d27bf89be8ecdd";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprspace = {
+      url = "github:KZDKM/Hyprspace";
+      inputs.hyprland.follows = "hyprland";
+    };
+    search-nixos-api.url = "github:anotherhadi/search-nixos-api";
+  };
+
+  outputs = inputs@{ nixpkgs, ... }: {
+    nixosConfigurations = {
+      kirby =
+        nixpkgs.lib.faggatron {
+          system = "x86_64-linux";
+          modules = [
+            {
+              nixpkgs.overlays = [ inputs.hyprpanel.overlay ];
+              _module.args = { inherit inputs; };
+            }
+            # inputs.nixos-hardware.nixosModules.omen-16-n0005ne check https://github.com/NixOS/nixos-hardware
+            inputs.home-manager.nixosModules.home-manager
+            inputs.stylix.nixosModules.stylix
+            ./hosts/faggatron/configuration.nix 
+          ];
+        };
+      # Jack is my server
+      jack = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          { _module.args = { inherit inputs; }; }
+          inputs.home-manager.nixosModules.home-manager
+          inputs.stylix.nixosModules.stylix
+          inputs.sops-nix.nixosModules.sops
+          inputs.nixarr.nixosModules.default
+          inputs.search-nixos-api.nixosModules.search-nixos-api
+          ./hosts/server/configuration.nix
+        ];
+      };
+    };
+  };
+}
